008036fccb7b44870d13de09b71e8de56cfa517a
Mrunal <mrunalkulkarni160170@gmail.com>
2025-11-27 23:19:23 +0530

    final changes

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,18 +1,28 @@
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
-import {Password} from "@convex-dev/auth/providers/Password";
-import { convexAuth } from "@convex-dev/auth/server";
+import Credentials from "@auth/core/providers/credentials";
+
+import { convexAuth } from "convex-helpers/server/auth";
 import { DataModel } from "./_generated/dataModel";
 
-const CustomPassword = Password<DataModel>({
-   profile(params){
-    return{
-      email:params.email as string,
-      name: params.name as string,
-      
-    };
-   },
-});
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [CustomPassword, GitHub, Google],
-});
+export const { auth, signIn, signOut, store, isAuthenticated } =
+  convexAuth<DataModel>({
+    providers: {
+      credentials: Credentials({
+        authorize: async (params) => {
+          return {
+            email: params.email as string,
+            name: params.name as string,
+          };
+        }
+      }),
+      github: GitHub({
+        clientId: process.env.AUTH_GITHUB_ID!,
+        clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      }),
+      google: Google({
+        clientId: process.env.AUTH_GOOGLE_ID!,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      })
+    }
+  });
